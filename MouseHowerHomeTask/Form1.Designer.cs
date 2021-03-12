0a0a016beaa54cc302653786ade4ffd4b0c2b2b3
Matin Rzaev <metin.rzaev@gmail.com>
2021-03-12 23:52:42 +0400

    Add project files.

---
+++ MouseHowerHomeTask/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace MouseHowerHomeTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FemaleBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FemaleBtn
+            // 
+            this.FemaleBtn.BackgroundImage = global::MouseHowerHomeTask.Properties.Resources.Femalee;
+            this.FemaleBtn.Location = new System.Drawing.Point(217, 310);
+            this.FemaleBtn.Name = "FemaleBtn";
+            this.FemaleBtn.Size = new System.Drawing.Size(100, 100);
+            this.FemaleBtn.TabIndex = 2;
+            this.FemaleBtn.UseVisualStyleBackColor = true;
+            this.FemaleBtn.MouseHover += new System.EventHandler(this.FemaleBtn_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FemaleBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button FemaleBtn;
+    }
+}
+
---
+++ MouseHowerHomeTask/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseHowerHomeTask
+{
+    public partial class Form1 : Form
+    {
+        Image image = Properties.Resources.Femalee;
+        public Form1()
+        {
+            InitializeComponent();
+            ChangeCursor();
+        }
+
+
+        private void ChangeCursor()
+        {
+            Bitmap btm = new Bitmap(Properties.Resources.male);
+            Cursor c = new Cursor(btm.GetHicon());
+            this.Cursor = c;
+        }
+
+       
+
+      
+        private void FemaleBtn_MouseHover(object sender, EventArgs e)
+        {
+            if(sender is Button bt)
+            {
+                bt.Dispose();
+                Button button = new Button();
+                
+                button.MouseHover += FemaleBtn_MouseHover;
+                button.Image = image;
+                button.AutoSize = true;
+
+
+                Random random = new Random();
+                int randomX = random.Next(this.Size.Width - 120);
+                int randomY = random.Next(this.Size.Height - 120);
+                button.Location = new Point(randomX, randomY);
+
+                this.Controls.Add(button);
+            }
+        }
+    }
+}
